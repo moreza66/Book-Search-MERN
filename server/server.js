c93a1f22326d7b65bc8df9997e06878801e6e978
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-12 21:44:22 -0600

    working on graphQL

--- server/server.js
+++ server/server.js
@@ -3,7 +3,8 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schema');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,11 +13,18 @@ app.use(express.json());
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use(routes);
 
