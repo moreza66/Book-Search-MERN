e99d5a189954a9c11682aa790800390c33b62600
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-12 22:09:34 -0600

    working on resolver

---
+++ server/schema/resolvers.js
@@ -0,0 +1,17 @@
+const { User } = require("../models");
+
+
+const resolvers = {
+    Query: {
+      me: async (parent, args, context) => {
+        if (context.user) {
+          const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+          return userData;
+        }
+      },
+    },
+  
+ 
+  };
+  
+  module.exports = resolvers;
