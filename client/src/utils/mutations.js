9b181446989f964f1c3f06df2f9890bd4ede0a20
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-12 23:33:35 -0600

    working on mutation

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,42 @@
+import gql from "graphql-tag";
+
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        # _id
+        bookId
+        authors
+        image
+        link
+        title
+        description
+      }
+    }
+  }
+`;
